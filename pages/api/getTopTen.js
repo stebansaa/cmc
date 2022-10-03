5e61f3e22a8ea9a4f6edf904cab73b4771773a6a
Esteban Saá <steban@gmail.com>
2022-10-03 18:43:53 -0500

    updated  temp json source

--- pages/api/getTopTen.js
+++ pages/api/getTopTen.js
@@ -1,6 +1,6 @@
 export default function handler(req,res) {
     const getData = async () => {
-        const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.CMC_API_KEY}`,
+        const response = await fetch(`https://raw.githubusercontent.com/evm20/JsonCMC/main/newjson.json`,
         {
             method: 'GET',
             headers: {
