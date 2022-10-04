f48ad6ffc5afcb18b2be9044efa7b74797cf1a9c
Esteban Saá <steban@gmail.com>
2022-10-03 19:32:08 -0500

    Update getTopTen.js

--- pages/api/getTopTen.js
+++ pages/api/getTopTen.js
@@ -1,6 +1,6 @@
 export default function handler(req,res) {
     const getData = async () => {
-        const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=100&CMC_PRO_API_KEY=e1704871-1291-4f16-8d47-f797f7e8d1c9`,
+        const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?id=Bitcoin,EOS,Telos,WAXP,USDT,&CMC_PRO_API_KEY=e1704871-1291-4f16-8d47-f797f7e8d1c9`,
         {
             method: 'GET',
             headers: {
